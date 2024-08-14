600beeb5755013fd525084f05cf573b16f86b009
Saatvik Tammishetty <saatvik.tammishetty@gmail.com>
2024-08-14 14:59:56 +0530

    Fixed Code Validation and Judgement

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,29 @@
+const dotenv = require("dotenv");
 const express = require("express");
-require("dotenv").config();
+// const cors = require("cors");
 const { DBConnection } = require("./configs/db");
-const authRoutes = require("./routes/auth");
-const problemRoutes = require("./routes/problem");
-const userRoutes = require("./routes/user");
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
+const { authRouter } = require("./routes/auth.js");
 
+dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(express.json());
-app.use(
-    cors({
-        origin: process.env.CLIENT_URL,
-        credentials: true,
-    })
-);
-app.use(cookieParser());
+// app.use(
+//   cors({
+//     origin: process.env.CLIENT_URL,
+//     credentials: true,
+//   })
+// );
 
 DBConnection();
 
 app.get("/", (req, res) => {
-    res.status(200).send("Server is Running");
+  res.status(200).send("Server is up and running!!");
 });
 
-app.use("/auth", authRoutes);
-app.use("/problems", problemRoutes);
-app.use("/user", userRoutes);
+app.use("/auth", authRouter);
 
 app.listen(port, () => {
-  console.log(`Server is Running on Port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
